ab4578771cfd6e3a57d66bec3d4958b3efc5142b
PanosGriziotis <panosgriz@gmail.com>
2022-06-24 13:15:08 +0300

    change main.py

--- system/main.py
+++ system/main.py
@@ -2,37 +2,36 @@
 import os
 from model import QuestionAnsweringSystem
 
-if __name__ == '__main__':
 
-    DATA_DIR = '../data/first_model_data'
-    TRAIN_FILE = 'answers.json'
-    MODEL_PATH = './models'
+DATA_DIR = '../data/first_model_data'
+TRAIN_FILE = 'answers.json'
+MODEL_PATH = './models'
 
-    if os.path.isdir(MODEL_PATH) == False:
-        os.mkdir(MODEL_PATH)
+if os.path.isdir(MODEL_PATH) == False:
+    os.mkdir(MODEL_PATH)
 
-    # initialize class
-    qa_system = QuestionAnsweringSystem(DATA_DIR, TRAIN_FILE)
+# initialize class
+qa_system = QuestionAnsweringSystem(DATA_DIR, TRAIN_FILE)
 
-    # get documents
-    docs =  qa_system.convert_to_haystack_format()
-    print ("pre-processing done")
+# get documents
+docs =  qa_system.convert_to_haystack_format()
+print ("pre-processing done")
 
-    # document store
-    ds = qa_system.create_document_store(docs)
-    print ('created document store')
+# document store
+ds = qa_system.create_document_store(docs)
+print ('created document store')
 
-    # fine-tune model on data
-    qa_system.fine_tune_reader_model (MODEL_PATH,n_epochs=1)
-    print ('fine-tuning done')
+# fine-tune model on data
+qa_system.fine_tune_reader_model (MODEL_PATH,n_epochs=1)
+print ('fine-tuning done')
 
-    # retriever & reader
-    retriever = qa_system.get_retriever(ds)
-    reader = qa_system.get_reader(MODEL_PATH)
+# retriever & reader
+retriever = qa_system.get_retriever(ds)
+reader = qa_system.get_reader(MODEL_PATH)
 
-    # pipeline 
-    pipe = qa_system.get_pipeline(reader, retriever)
+# pipeline 
+pipe = qa_system.get_pipeline(reader, retriever)
 
-    # get answers dictionary
-    answers  = qa_system.get_answers("Μπορώ να κάνω δεύτερη δόση με διαφορετικό εμβόλιο;",pipe)
-    print(answers)
+# get answers dictionary
+answers  = qa_system.get_answers("Μπορώ να κάνω δεύτερη δόση με διαφορετικό εμβόλιο;",pipe)
+print(answers)
